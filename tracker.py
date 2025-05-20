fa204a3499b515c1f3c8d4d39550ce93cc1eaaee
Ethan-Blesch <eblesch19@student.oprfhs.org>
2025-05-20 11:42:39 -0500

    Update tracker.py
    Worked on some more functionality so it can be implemented into the Angr-Management plugin

--- tracker.py
+++ tracker.py
@@ -1,9 +1,13 @@
 import angr
 import claripy
 
+
+#stdin setup is pretty sketch at the moment and probably only works for the short test binary I've been using (if that)
+#TODO: figure out a cleaner way to do symbolic stdin 
 proj = angr.Project("test_program", auto_load_libs = False)
 sym_val = claripy.BVS("SymbolicInput", 8)
 stream = angr.SimFileStream(name="stdin", content=sym_val, has_end=True);
+
 
 
 def disasm(state):
@@ -16,20 +20,16 @@ def disasm(state):
 
 
 def symAddr(state):
-    #Check if the address we're writing to is user-controlled
     if isinstance(state.inspect.mem_write_address, claripy.ast.bv.BV) and state.inspect.mem_write_address.symbolic:
         return True;
     return False;
-    
 
 def symData(state):
-    #Check if what we're writing is user-controlled
     if isinstance(state.inspect.mem_write_expr, claripy.ast.bv.BV) and state.inspect.mem_write_expr.symbolic:
         return True;
     return False;
 
 def shouldTrack(state):
-    #If a memory write's data and address are both not user-controlled, it's probably nothing interesting
     if symAddr(state) or symData(state):
         return True;
     return False;
@@ -39,22 +39,29 @@ def shouldTrack(state):
 
 class MemoryWrite:
     def __init__(self, state):
-
-        #TODO: Properly implement length and size.
-        
         self.startAddr = state.solver.min(state.inspect.mem_write_address);
         self.endAddr = state.solver.max(state.inspect.mem_write_address);
-        self.size = state.solver.eval(state.inspect.mem_write_length);
+        self.len = state.solver.eval(state.inspect.mem_write_length);
         self.symData = symData(state);
         self.rip = state.solver.eval(state.regs.rip);
         self.state = state
     
+    def __repr__(self):
+        return "<Memory write at RIP = " + hex(self.rip) + ">"
+
+
+    def __eq__(self, other):
+        if self.startAddr == other.startAddr and self.endAddr == other.endAddr and self.len == other.len and self.symData == other.symData and self.rip == other.rip:
+            return True
+        return False;
+
+
     def debugPrint(self):
         print(f"MEMORY WRITE AT {hex(self.rip)}:")
         print(f"\tMemory details:")
         print(f"\t\tStart: {hex(self.startAddr)}")
         print(f"\t\tEnd:   {hex(self.endAddr)}")
-        print(f"\t\tSize:  {hex(self.size)}")
+        print(f"\t\tSize:  {hex(self.len)}")
         print(f"\t\tSymbolic data: {self.symData}")
         print(f"\tState details:")
         print(self.state.solver.constraints)
@@ -66,29 +73,35 @@ class MemoryWrite:
 
 
 def write_bp(state):
-
-    # Our breakpoint function is mostly just for testing right now, so there's a lot of debug printouts that I have in there for no reason 
     
     if not shouldTrack(state):
         return
-
-    
+        
     write = MemoryWrite(state)
     write.debugPrint()
     disasm(state)
-    
-    print(hex(state.solver.eval(state.regs.rsi)))
-    print(state.memory.load(state.regs.rip, 8))
+    state.globals["writes"].append(write)
 
 
+def find_writes(proj):
 
-entry_state = proj.factory.entry_state(stdin=stream)
-print(type(entry_state))
-entry_state.inspect.b('mem_write', when=angr.BP_BEFORE, action=write_bp)
-simgr = proj.factory.simgr(entry_state);
+    entry_state = proj.factory.entry_state(stdin=stream)
+    entry_state.inspect.b('mem_write', when=angr.BP_BEFORE, action=write_bp)
 
-simgr.explore()
+    entry_state.globals["writes"] = []
+    simgr = proj.factory.simgr(entry_state);
 
+    simgr.explore()
+    print(simgr)
 
+    writes = []
+    for state in simgr.deadended:
+        for write in state.globals["writes"]:
+            if write in writes:
+                print("DUPLICATE");
+            else:
+                writes.append(write)
+
+    return writes
 
 
